76e9674dabb61817d441b9a01f6691514c9de755
Dario Chinea <dariocd0808@gmail.com>
2024-02-22 12:06:34 +0000

    Add feature

--- src/main/java/com/jwt/controller/UsuarioController.java
+++ src/main/java/com/jwt/controller/UsuarioController.java
@@ -4,10 +4,7 @@ import com.jwt.entity.Usuario;
 import com.jwt.service.UsuarioService;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -20,5 +17,10 @@ public class UsuarioController {
     @PostMapping("/register")
     public Usuario registerUsuario (@RequestBody Usuario usuario) {
         return usuarioService.registerUsuario(usuario);
+    }
+
+    @GetMapping()
+    public String prueba (@RequestParam (value = "texto") String texto) {
+        return texto;
     }
 }
